07c215d4ea794bad04a9d3e31579598616ed2ed5
Mr atash <64045922+aliarzani@users.noreply.github.com>
2026-09-04 07:34:56 -0700

    feat: enable transfer enhancements

--- src/main.tsx
+++ src/main.tsx
@@ -4,18 +4,16 @@ import App from './App'
 import { installTransactionSafetyBridge } from './features/transactionSafetyBridge'
 import { rescheduleEnabledReminders } from './features/reminders'
 import { installAppFeaturesBridge } from './features/appFeaturesBridge'
+import { installTransferEnhancements } from './features/transferEnhancements'
 import './index.css'
 
 installTransactionSafetyBridge()
 installAppFeaturesBridge()
+installTransferEnhancements()
 void rescheduleEnabledReminders().catch(() => undefined)
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+root.render(<React.StrictMode><App /></React.StrictMode>)
 
 requestAnimationFrame(() => {
   requestAnimationFrame(() => {
