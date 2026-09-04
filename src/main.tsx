0a9290716c615dbd322ff6fbf19cfec6dab35749
Mr atash <64045922+aliarzani@users.noreply.github.com>
2026-09-03 18:20:41 -0700

    feat: restore enabled reminders on app startup

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { installNotificationSettingsBridge } from './features/notificationSettingsBridge'
+import { rescheduleEnabledReminders } from './features/reminders'
 import './index.css'
 
 installNotificationSettingsBridge()
+void rescheduleEnabledReminders().catch(() => undefined)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
