663afe3fdbc7974e9e88269e595468725ae2ce3f
Mr atash <64045922+aliarzani@users.noreply.github.com>
2026-09-03 18:25:00 -0700

    feat: connect transfer safety guard at app startup

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { installNotificationSettingsBridge } from './features/notificationSettingsBridge'
+import { installTransactionSafetyBridge } from './features/transactionSafetyBridge'
 import { rescheduleEnabledReminders } from './features/reminders'
 import './index.css'
 
 installNotificationSettingsBridge()
+installTransactionSafetyBridge()
 void rescheduleEnabledReminders().catch(() => undefined)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
