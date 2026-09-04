dba27ee088deff999997a95d597e309380ceccb2
Mr atash <64045922+aliarzani@users.noreply.github.com>
2026-09-04 12:03:33 -0700

    fix: install native exchange device bridge

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import { installTransactionSafetyBridge } from './features/transactionSafetyBrid
 import { rescheduleEnabledReminders } from './features/reminders'
 import { installAppFeaturesBridge } from './features/appFeaturesBridge'
 import { installTransferEnhancements } from './features/transferEnhancements'
+import { installExchangeDeviceFixes } from './features/exchangeDeviceFixes'
 import './index.css'
 
 installTransactionSafetyBridge()
 installAppFeaturesBridge()
 installTransferEnhancements()
+installExchangeDeviceFixes()
 void rescheduleEnabledReminders().catch(() => undefined)
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
