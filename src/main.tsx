498673870f4420724a774b87835e69e9f961b978
Mr atash <64045922+aliarzani@users.noreply.github.com>
2026-09-03 18:42:16 -0700

    Expose reminders from the app home UI

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import App from './App'
 import { installNotificationSettingsBridge } from './features/notificationSettingsBridge'
 import { installTransactionSafetyBridge } from './features/transactionSafetyBridge'
 import { rescheduleEnabledReminders } from './features/reminders'
+import { installReminderBridge } from './features/reminderBridge'
 import './index.css'
 
 installNotificationSettingsBridge()
 installTransactionSafetyBridge()
+installReminderBridge()
 void rescheduleEnabledReminders().catch(() => undefined)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
