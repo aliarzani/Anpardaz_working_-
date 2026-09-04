7b19ae72bf36246e557217523c4bac55fa32ebba
Mr atash <64045922+aliarzani@users.noreply.github.com>
2026-09-03 20:45:59 -0700

    Use unified in-app feature controls for financial center, reminders and notification settings

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { installNotificationSettingsBridge } from './features/notificationSettingsBridge'
 import { installTransactionSafetyBridge } from './features/transactionSafetyBridge'
 import { rescheduleEnabledReminders } from './features/reminders'
-import { installReminderBridge } from './features/reminderBridge'
-import { installFinancialCenterBridge } from './features/financialCenterBridge'
+import { installAppFeaturesBridge } from './features/appFeaturesBridge'
 import './index.css'
 
-installNotificationSettingsBridge()
 installTransactionSafetyBridge()
-installReminderBridge()
-installFinancialCenterBridge()
+installAppFeaturesBridge()
 void rescheduleEnabledReminders().catch(() => undefined)
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
