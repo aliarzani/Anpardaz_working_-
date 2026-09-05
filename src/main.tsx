daa85378b9a676451d6a838398b3f748e1d15626
Mr atash <64045922+aliarzani@users.noreply.github.com>
2026-09-04 17:42:52 -0700

    feat: enable service and exchange personalization from build 122

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,16 @@ import { rescheduleEnabledReminders } from './features/reminders'
 import { installAppFeaturesBridge } from './features/appFeaturesBridge'
 import { installTransferEnhancements } from './features/transferEnhancements'
 import { installExchangeDeviceFixes } from './features/exchangeDeviceFixes'
+import { installServiceCustomizationV3 } from './features/serviceCustomizationV3'
+import { installExchangeServiceCustomizationV2 } from './features/exchangeServiceCustomizationV2'
 import './index.css'
 
 installTransactionSafetyBridge()
 installAppFeaturesBridge()
 installTransferEnhancements()
 installExchangeDeviceFixes()
+installServiceCustomizationV3()
+installExchangeServiceCustomizationV2()
 void rescheduleEnabledReminders().catch(() => undefined)
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
